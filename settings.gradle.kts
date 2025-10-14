8902fa219aba1a7a78d29351d715dff266bcc7a2
Intybyte <vaan.testwork@gmail.com>
2025-10-14 12:21:52 +0200

    Move stuff to appropriate directory

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,14 @@
 rootProject.name = "BibleReadPlugin"
 
 include("api")
+
 include("bukkit")
+project(":bukkit").projectDir = file("./bukkit_main/bukkit")
+
 include("spigot")
+project(":spigot").projectDir = file("./bukkit_main/spigot")
+
 include("paper")
+project(":paper").projectDir = file("./bukkit_main/paper")
+
 include("sponge8")
